3ea332922a17fd019cc2e0bbbb3ade8df2648774
MurilloTavares <murillo_eum@hotmail.com>
2018-12-12 17:52:00 -0300

    Tela MainActivity com caminho pra CadastrarActivity

--- app/src/main/java/br/edu/ifpb/academycalendar/MainActivity.java
+++ app/src/main/java/br/edu/ifpb/academycalendar/MainActivity.java
@@ -1,13 +1,31 @@
 package br.edu.ifpb.academycalendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setCadastrarListener();
+    }
+
+    public void setCadastrarListener(){
+        btnCadastrar = (Button) findViewById(R.id.btn_cadastrar);
+        btnCadastrar.setOnClickListener( new View.OnClickListener () {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(".CadastrarActivity");
+                startActivity(intent);
+            }
+        });
     }
 }
