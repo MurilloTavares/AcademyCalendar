3ac672727674f2ceb9e3b73c1c96c32406b8706a
MurilloTavares <murillo_eum@hotmail.com>
2018-12-19 19:11:03 -0300

    Adicionando tela de criar turma

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -78,15 +78,15 @@ public final class R {
         public static final int gone = 0x7f070047;
         public static final int invisible = 0x7f070052;
         public static final int left = 0x7f070054;
-        public static final int packed = 0x7f070066;
-        public static final int parent = 0x7f070067;
-        public static final int percent = 0x7f070069;
-        public static final int right = 0x7f070070;
-        public static final int spread = 0x7f070089;
-        public static final int spread_inside = 0x7f07008a;
-        public static final int start = 0x7f07008f;
-        public static final int top = 0x7f07009e;
-        public static final int wrap = 0x7f0700b2;
+        public static final int packed = 0x7f07006e;
+        public static final int parent = 0x7f07006f;
+        public static final int percent = 0x7f070071;
+        public static final int right = 0x7f070078;
+        public static final int spread = 0x7f070091;
+        public static final int spread_inside = 0x7f070092;
+        public static final int start = 0x7f070097;
+        public static final int top = 0x7f0700a6;
+        public static final int wrap = 0x7f0700be;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/br/edu/ifpb/academycalendar/TurmasActivity.java
+++ app/src/main/java/br/edu/ifpb/academycalendar/TurmasActivity.java
@@ -1,7 +1,10 @@
 package br.edu.ifpb.academycalendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
@@ -15,11 +18,20 @@ public class TurmasActivity extends AppCompatActivity {
     private List<String> listDataHeader;
     private HashMap<String, List<String>> listHash;
 
+    private Button btnCancelar;
+    private Button btnCriar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turmas);
 
+        setExpandableListView();
+        setCancelarListener();
+        setCriarListener();
+    }
+
+    private void setExpandableListView(){
         listView = (ExpandableListView) findViewById(R.id.expList);
         initData();
         listAdapter = new MyListAdapter(this, listDataHeader, listHash);
@@ -44,6 +56,27 @@ public class TurmasActivity extends AppCompatActivity {
 
         listHash.put(listDataHeader.get(0), turma1);
         listHash.put(listDataHeader.get(1), turma2);
+    }
+
+    public void setCancelarListener(){
+        btnCancelar = (Button) findViewById(R.id.turmas_btn_cancelar);
+        btnCancelar.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+    }
+
+    public void setCriarListener(){
+        btnCriar = (Button) findViewById(R.id.turmas_btn_criar);
+        btnCriar.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(".NovaTurmaActivity");
+                startActivity(intent);
+            }
+        });
     }
 
 }
